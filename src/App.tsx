2745fa63feeb2fae4e4929616f81df2e766f3c6c
gokuljs <gokuljs029@gmail.com>
2022-11-03 20:56:01 +0530

    phase 1 of firebase deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,58 @@
 import React, { useState } from "react";
-import { styled } from "@mui/material";
+import { styled, Grid } from "@mui/material";
 import HomePage from "./pages/HomePage";
+import fireBase from "./assests/svg/firebase.svg";
 
 const MainContainer = styled("div")`
   width: 100%;
-  min-width: 100vh;
+  min-height: 100vh;
 `;
 
+const NavBarContainer = styled(Grid)`
+  min-height: 50px;
+  padding: 1rem 5rem;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  .deploy {
+    padding: 0.7rem 1rem;
+    text-transform: capitalize;
+    font-family: "Roboto", sans-serif;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(
+      90deg,
+      rgba(88, 43, 255, 1) 24%,
+      rgba(156, 17, 255, 1) 75%,
+      rgba(219, 13, 245, 1) 100%
+    );
+
+    color: #fff;
+    border: 1px solid #db46fc;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    transition: all 0.3s ease-in-out;
+    :hover {
+      background: transparent;
+      color: rgba(88, 43, 255, 1);
+    }
+  }
+  .fireBase-Image {
+    height: 1.5rem;
+  }
+`;
 function App() {
   const [jsonData, setJsonData] = useState([]);
   console.log(jsonData, "ssss");
   return (
     <MainContainer>
+      <NavBarContainer container lg={12} item>
+        <button className="deploy">
+          <img src={fireBase} className="fireBase-Image" alt="firebAse-logo" />
+          deploy to firebase
+        </button>
+      </NavBarContainer>
       <HomePage setJsonData={setJsonData} jsonData={jsonData} />
       {Array.isArray(jsonData) &&
         jsonData.length > 0 &&
